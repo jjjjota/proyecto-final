7238a89702ba1c0f9fe87f8bf7b487c5a7859f51
jjjjota <jajopia@uc.cl>
2018-09-13 15:48:14 -0300

    Add: header.php

--- assets/js/app.js
+++ assets/js/app.js
@@ -12998,7 +12998,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_waypoints_lib_jquery_waypoints_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_waypoints_lib_jquery_waypoints_min_js__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_waypoints_lib_shortcuts_inview_min_js__ = __webpack_require__(7);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_waypoints_lib_shortcuts_inview_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_waypoints_lib_shortcuts_inview_min_js__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__functions_js__ = __webpack_require__(21);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__functions_js__ = __webpack_require__(8);
 // Dependencies
 
 
@@ -17072,25 +17072,7 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
 !function(){"use strict";function t(){}function e(t){this.options=i.Adapter.extend({},e.defaults,t),this.axis=this.options.horizontal?"horizontal":"vertical",this.waypoints=[],this.element=this.options.element,this.createWaypoints()}var i=window.Waypoint;e.prototype.createWaypoints=function(){for(var t={vertical:[{down:"enter",up:"exited",offset:"100%"},{down:"entered",up:"exit",offset:"bottom-in-view"},{down:"exit",up:"entered",offset:0},{down:"exited",up:"enter",offset:function(){return-this.adapter.outerHeight()}}],horizontal:[{right:"enter",left:"exited",offset:"100%"},{right:"entered",left:"exit",offset:"right-in-view"},{right:"exit",left:"entered",offset:0},{right:"exited",left:"enter",offset:function(){return-this.adapter.outerWidth()}}]},e=0,i=t[this.axis].length;i>e;e++){var n=t[this.axis][e];this.createWaypoint(n)}},e.prototype.createWaypoint=function(t){var e=this;this.waypoints.push(new i({context:this.options.context,element:this.options.element,enabled:this.options.enabled,handler:function(t){return function(i){e.options[t[i]].call(e,i)}}(t),offset:t.offset,horizontal:this.options.horizontal}))},e.prototype.destroy=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].destroy();this.waypoints=[]},e.prototype.disable=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].disable()},e.prototype.enable=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].enable()},e.defaults={context:window,enabled:!0,enter:t,entered:t,exit:t,exited:t},i.Inview=e}();
 
 /***/ }),
-/* 8 */,
-/* 9 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 10 */,
-/* 11 */,
-/* 12 */,
-/* 13 */,
-/* 14 */,
-/* 15 */,
-/* 16 */,
-/* 17 */,
-/* 18 */,
-/* 19 */,
-/* 20 */,
-/* 21 */
+/* 8 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -17111,6 +17093,12 @@ var addClassRemoveItFromSiblings = function addClassRemoveItFromSiblings($curren
 
 // Export functions
 
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
--- header.php
+++ header.php
@@ -9,31 +9,40 @@
 </head>
 <body>
 
-<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
-<?php } ?>
-
 <!-- Navigation bar -->
-<!-- <nav class="navbar navbar-dark navbar-expand-lg">
-  <a href="<?php //home_url() ?>">
-    <img class="navbar-brand" src="<?php //get_field('logo', 'options')['url'] ?>" alt="krita logo">
+<nav class="navbar navbar-dark navbar--krita navbar-expand-md">
+  <a href="<?php echo home_url() ?>">
+    <img class="navbar-brand" src="<?php echo get_field( 'logo' )['url'] ?>" alt="krita logo">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <i class="fas fa-bars"></i>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <?php
-    // wp_nav_menu( array(
-    //   'theme_location' => 'header-nav',
-    //   'container'      => false,
-    //   'menu_class'     => 'navbar-nav ml-auto',
-    //   'fallback_cb'    => '__return_false',
-    //   'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-    //   'depth'          => 2,
-    //   'walker'         => new bootstrap_4_walker_nav_menu()
-    // ) );
-    ?>
-
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Download</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Documentation</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Participate
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Art Gallery</a>
+          <a class="dropdown-item" href="#">Get Involved</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Donate!</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Shop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+      </li>
+    </ul>
   </div>
-</nav> -->
+</nav>

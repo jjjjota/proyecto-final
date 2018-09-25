eeaf4ef76cf03f4733f2888462de82c0be4da2fb
jjjjota <jajopia@uc.cl>
2018-09-25 14:41:47 -0300

    Change: Page 404 style

--- 404.php
+++ 404.php
@@ -2,12 +2,13 @@
 
 <section class="page404">
 
-  <div class="page404__content">
+  <div class="page404__content center--1000">
+    <i class="page404__icon fas fa-times"></i>
     <h1>
       404 error:
     </h1>
     <h2>
-      This page doesn't exist
+      This page doesn't exists
     </h2>
   </div>
 </section>

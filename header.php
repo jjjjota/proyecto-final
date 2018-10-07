67e16d7412d7a3408361d26f304801e013b9e4d6
jjjjota <jajopia@uc.cl>
2018-10-07 13:15:28 -0300

    feature: google analytics integration

--- header.php
+++ header.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo('charset') ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126975892-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-126975892-1');
+  </script>
+
+	<meta charset="<?php bloginfo('charset') ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<?php get_template_part('_includes/iOS', 'icons') ?>
 	<?php wp_head() ?>
 </head>
